096f0ecf8924179a1a835cd912f3f7170779c944
Amirul Zafrin <amirulzafrin98@gmail.com>
2022-08-12 15:20:55 +0800

    Added Leetcode - Buy Sell stock solution

---
+++ Leetcode/buysellstock.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minPrice = prices[0]
+        maxPrice = 0
+
+        for p in prices:
+            if p < minPrice:
+                minPrice = p
+            elif p - minPrice > maxPrice:
+                maxPrice = p - minPrice
+
+        return maxPrice
